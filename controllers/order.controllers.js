3b49bbab93867cb5f9d2ac8005f20a223c724449
Soudipta Sarkar <soudiptass26@gmail.com>
2026-08-27 10:46:55 +0530

    add controller for update order status

--- controllers/order.controllers.js
+++ controllers/order.controllers.js
@@ -64,6 +64,13 @@ export const placeOrder = async (req, res) => {
       shopOrders,
     });
 
+    await newOrder.populate(
+      "shopOrders.shopOrderItems.item",
+      "name image price"
+    );
+
+    await newOrder.populate("shopOrders.shop", "name");
+
     return res.status(201).json(newOrder);
   } catch (error) {
     return res.status(500).json({ message: `Place order error ${error}` });
@@ -88,9 +95,43 @@ export const getMyOrders = async (req, res) => {
         .populate("user")
         .populate("shopOrders.shopOrderItems.item", "name image price ");
 
-      return res.status(200).json(orders);
+      const filteredOrder = orders.map((order) => ({
+        _id: order._id,
+        paymentMethod: order.paymentMethod,
+        user: order.user,
+        shopOrders: order.shopOrders.find((o) => o.owner._id == req.userId),
+        createdAt: order.createdAt,
+        deliveryAddress: order.deliveryAddress,
+      }));
+
+      return res.status(200).json(filteredOrder);
     }
   } catch (error) {
     return res.status(500).json({ message: `get user order error ${error}` });
   }
 };
+
+
+export const updateOrderStatus = async (req, res) =>{
+  try {
+    const {orderId, shopId} = req.params;
+    const {status} = req.body;
+    const order =await Order.findById(orderId);
+
+    const shopOrder = order.shopOrders.find(o => o.shop == shopId );
+    if(!shopOrder){
+      return res.status(400).json({message: "shop order not found"});
+    }
+
+    shopOrder.status = status;
+
+    // await shopOrder.save();
+    await order.save()
+    
+    return res.status(200).json(shopOrder.status);
+
+  } catch (error) {
+    return res.status(500).json({ message: `order status error ${error}` });
+  }
+
+}
--- routes/order.routes.js
+++ routes/order.routes.js
@@ -1,13 +1,13 @@
 import express from "express";
 import isAuth from "../middlewares/isAuth.js";
-import { getMyOrders, placeOrder } from "../controllers/order.controllers.js";
+import { getMyOrders, placeOrder, updateOrderStatus } from "../controllers/order.controllers.js";
 
 
 const orderRouter = express.Router();
 
 orderRouter.post("/place-order", isAuth, placeOrder);
 orderRouter.get("/my-orders", isAuth, getMyOrders);
+orderRouter.post("/update-status/:orderId/:shopId", isAuth, updateOrderStatus)
 
-
-
+// 6.32min
 export default orderRouter;
